37c50c4d65cfd3f9202abd7277228f7f29615038
spacemindhiro <spacemind.hiro@gmail.com>
2026-03-25 07:24:03 +0900

    demoをホワイトリストに追加

--- types/validator.ts
+++ types/validator.ts
@@ -104,7 +104,7 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
 // Validate ../../app/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
-  const handler = {} as typeof import("@/app/demo/page.js")
+  const handler = {} as typeof import("@/app/page.js")
   type __Check = __IsExpected<typeof handler>
   // @ts-ignore
   type __Unused = __Check
